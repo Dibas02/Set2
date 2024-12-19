717d37060742675db2dc4168e8cc2990c44a2aef
Dibas Talukder <debal1952@gmail.com>
2024-12-19 19:39:23 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+a = {1, 2, 3, 4, 5, 7, 9}
+b = {1, 3, 5, 7, 8, 10}
+
+print(a.intersection(b))
+print(a.union(b))
+print(a.difference(b))
+print(a.symmetric_difference(b))
+
+c = {"Red", "Orange", "Yellow", "Blue", "Purple"}
+d = {"Yellow", "Pink", "Green", "Red", "Purple", "Black"}
+
+print(c.intersection(d))
+print(c.union(d))
+print(c.difference(d))
+print(c.symmetric_difference(d))
